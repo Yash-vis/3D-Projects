dcaa6380de5ee4a3e18e1598b83223760c7ce452
Yash-vis <yash20vi@gmail.com>
2024-11-03 21:52:24 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,44 @@
+import { Suspense } from 'react';
+import './App.css';
+import { Canvas } from '@react-three/fiber';
+import { Environment, OrbitControls } from '@react-three/drei';
+import Scene from './components/Scene';
+import Navbar from './components/Navbar';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/about",
+    element: <About />
+  },
+  {
+    path: "/contact",
+    element: <Contact />
+  }
+]);
+
+function App() {
+  
+  return (
+    <RouterProvider router={router}>
+      <Navbar /> {/* Make sure Navbar is inside RouterProvider */}
+      <Canvas>
+        <ambientLight />
+        <OrbitControls />
+        <Suspense fallback={null}>
+          <Scene />
+        </Suspense>
+        <Environment preset="sunset" />
+      </Canvas>
+    </RouterProvider>
+  );
+}
+
+export default App;
